5203f946c653303c45cb85a7096d4eb57858bb0f
Mark Robson <mark.o.robson@gmail.com>
2018-11-24 15:49:32 +0000

    refactor

--- cards/main.go
+++ cards/main.go
@@ -13,53 +13,12 @@ type Card struct {
 	value []int
 }
 
-// Player - will have a name and a hand
-type Player struct {
-	name string
-	hand []Card
-}
-
 // Deck - 52 cards, random order
 type Deck []Card
 
 var names = []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 var suites = []string{"Hearts", "Spades", "Diamonds", "Clubs"}
 var values = [][]int{{1, 11}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {10}, {10}, {10}}
-
-func main() {
-	deck := createDeck()
-	dealer := createPlayer("Dealer")
-	player1 := createPlayer("Mark")
-
-	fmt.Printf("%v cards left\n", len(deck))
-
-	dealPlayer(&dealer, &deck)
-	dealPlayer(&player1, &deck)
-	dealPlayer(&dealer, &deck)
-	dealPlayer(&player1, &deck)
-
-	dealerMin, dealerMax := getPlayerTotal(&dealer)
-	player1Min, player1Max := getPlayerTotal(&player1)
-
-	fmt.Printf("%v cards left\n", len(deck))
-	fmt.Printf("Dealer min: %v, max: %v\n", dealerMin, dealerMax)
-	fmt.Printf("Player min: %v, max: %v\n", player1Min, player1Max)
-
-	if player1Min < 16 || player1Max < 16 {
-		dealPlayer(&player1, &deck)
-	}
-
-	if dealerMin < 16 || dealerMax < 16 {
-		dealPlayer(&dealer, &deck)
-	}
-
-	dealerMin, dealerMax = getPlayerTotal(&dealer)
-	player1Min, player1Max = getPlayerTotal(&player1)
-
-	fmt.Printf("%v cards left\n", len(deck))
-	fmt.Printf("Dealer min: %v, max: %v\n", dealerMin, dealerMax)
-	fmt.Printf("Player min: %v, max: %v\n", player1Min, player1Max)
-}
 
 func createDeck() Deck {
 	cards := make(Deck, 0)
@@ -79,19 +38,15 @@ func createDeck() Deck {
 	return cards
 }
 
-func createPlayer(name string) Player {
-	return Player{name: name}
+// Player - will have a name and a hand
+type Player struct {
+	name string
+	hand []Card
 }
 
-func dealPlayer(player *Player, deck *Deck) {
-	card := (*deck)[len((*deck))-1]
-	(*player).hand = append((*player).hand, card)
-	*deck = (*deck)[:len((*deck))-1]
-}
-
-func getPlayerTotal(player *Player) (int, int) {
+func (p *Player) getMinMax() (min, max int) {
 	minTotal, maxTotal := 0, 0
-	for _, card := range (*player).hand {
+	for _, card := range p.hand {
 		minTotal += card.value[0]
 		if len(card.value) == 1 {
 			maxTotal += card.value[0]
@@ -100,4 +55,54 @@ func getPlayerTotal(player *Player) (int, int) {
 		}
 	}
 	return minTotal, maxTotal
+}
+
+func (p *Player) twist(card Card) {
+	p.hand = append(p.hand, card)
+}
+
+func createPlayer(name string) Player {
+	return Player{name: name}
+}
+
+func dealPlayer(player *Player, deck *Deck) {
+	card := (*deck)[len((*deck))-1]
+	// (*player).hand = append((*player).hand, card)
+	player.twist(card)
+	*deck = (*deck)[:len((*deck))-1]
+}
+
+func main() {
+	deck := createDeck()
+	dealer := createPlayer("Dealer")
+	player1 := createPlayer("Mark")
+
+	fmt.Printf("%v cards left\n", len(deck))
+
+	dealPlayer(&dealer, &deck)
+	dealPlayer(&player1, &deck)
+	dealPlayer(&dealer, &deck)
+	dealPlayer(&player1, &deck)
+
+	dealerMin, dealerMax := dealer.getMinMax()
+	player1Min, player1Max := player1.getMinMax()
+
+	fmt.Printf("%v cards left\n", len(deck))
+	fmt.Printf("Dealer min: %v, max: %v\n", dealerMin, dealerMax)
+	fmt.Printf("Player min: %v, max: %v\n", player1Min, player1Max)
+
+	if player1Min < 16 || player1Max < 16 {
+		dealPlayer(&player1, &deck)
+	}
+
+	if dealerMin < 16 || dealerMax < 16 {
+		dealPlayer(&dealer, &deck)
+	}
+
+	dealerMin, dealerMax = dealer.getMinMax()
+	player1Min, player1Max = player1.getMinMax()
+
+	fmt.Printf("%v cards left\n", len(deck))
+	fmt.Printf("Dealer min: %v, max: %v\n", dealerMin, dealerMax)
+	fmt.Printf("Player min: %v, max: %v\n", player1Min, player1Max)
 }
